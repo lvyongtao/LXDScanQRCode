8f9dc5360c6a2d89160967f96e8f6336395e464c
Sindri Lin <634893985@qq.com>
2015-11-01 16:29:05 +0800

    Sindri Lin

--- LXDScanQRCode/ViewController.m
+++ LXDScanQRCode/ViewController.m
@@ -7,8 +7,13 @@
 //
 
 #import "ViewController.h"
+#import "LXDScanView.h"
+#import "LXDScanCodeController.h"
+#import <AVFoundation/AVFoundation.h>
 
-@interface ViewController ()
+@interface ViewController ()<LXDScanViewDelegate, LXDScanCodeControllerDelegate>
+
+@property (nonatomic, strong) LXDScanView * scanView;
 
 @end
 
@@ -16,12 +21,80 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidDisappear:(BOOL)animated
+{
+    [super viewDidDisappear: animated];
+    [self.scanView stop];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
+
+- (void)dealloc
+{
+    [self.scanView stop];
+}
+
+
+#pragma mark - event
+- (IBAction)scan:(id)sender
+{
+    [self.view addSubview: self.scanView];
+    [self.scanView start];
+}
+
+- (IBAction)jumpToScan:(id)sender
+{
+    [self.scanView removeFromSuperview];
+    LXDScanCodeController * scanCodeController = [LXDScanCodeController scanCodeController];
+    scanCodeController.scanDelegate = self;
+    [self.navigationController pushViewController: scanCodeController animated: YES];
+}
+
+
+#pragma mark - getter
+/**
+ *  懒加载扫描view
+ */
+- (LXDScanView *)scanView
+{
+    if (!_scanView) {
+        _scanView = [LXDScanView scanViewShowInController: self];
+    }
+    return _scanView;
+}
+
+
+#pragma mark - LXDScanViewDelegate
+/**
+ *  返回扫描结果
+ */
+- (void)scanView:(LXDScanView *)scanView codeInfo:(NSString *)codeInfo
+{
+    NSURL * url = [NSURL URLWithString: codeInfo];
+    if ([[UIApplication sharedApplication] canOpenURL: url]) {
+        [[UIApplication sharedApplication] openURL: url];
+    } else {
+        UIAlertView * alertView = [[UIAlertView alloc] initWithTitle: @"警告" message: [NSString stringWithFormat: @"%@:%@", @"无法解析的二维码", codeInfo] delegate: nil cancelButtonTitle: @"确定" otherButtonTitles: nil];
+        [alertView show];
+    }
+}
+
+
+#pragma mark - LXDScanCodeControllerDelegate
+- (void)scanCodeController:(LXDScanCodeController *)scanCodeController codeInfo:(NSString *)codeInfo
+{
+    NSURL * url = [NSURL URLWithString: codeInfo];
+    if ([[UIApplication sharedApplication] canOpenURL: url]) {
+        [[UIApplication sharedApplication] openURL: url];
+    } else {
+        UIAlertView * alertView = [[UIAlertView alloc] initWithTitle: @"警告" message: [NSString stringWithFormat: @"%@:%@", @"无法解析的二维码", codeInfo] delegate: nil cancelButtonTitle: @"确定" otherButtonTitles: nil];
+        [alertView show];
+    }
+}
+
 
 @end
